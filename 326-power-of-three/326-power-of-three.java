bbc473ce3784ab047253ab7211ad2fde974cbc06
surya dharamgudi <suryablaster@gmail.com>
2022-08-25 18:28:49 +0530

    Time: 19 ms (80.11%), Space: 47.6 MB (31.71%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        
+       if(n>0){
+           while(n%3==0){
+               n=n/3;
+           }
+           if(n==1) return true;
+       }
+        
+        
+        return false;
+        
+    }
+}
